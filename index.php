81eadac68b4cc880a7fb5901057d4b49e60a7718
Akalanka Ekanayake <akalankauk@users.noreply.github.com>
2017-07-22 12:14:31 +0530

    Update index.php
    SSL issue fixed!

--- index.php
+++ index.php
@@ -163,7 +163,7 @@ a:hover {
         </form>
     </div>
     <script type="text/javascript"
-        src="http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js"></script>
+        src="https://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js"></script>
     <script type="text/javascript">
 // jQuery Document
 $(document).ready(function(){
